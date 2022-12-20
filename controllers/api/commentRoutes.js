4bcd2b93763e980386b3593fced723583a57e421
pbp66 <{ID}+{username}@users.noreply.github.com>
2022-12-19 23:02:36 -0500

    added template code for comment routes. WIP

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,4 +7,19 @@ router.get("/:post_id", async (req, res) => {
 	console.log(`Post ID: ${req.params.post_id}`);
 });
 
+router.post("/", async (req, res) => {
+	console.log("API comment route");
+	console.log(`Post ID: ${req.params.post_id}`);
+});
+
+router.put("/:id", async (req, res) => {
+	console.log("API comment route");
+	console.log(`Post ID: ${req.params.post_id}`);
+});
+
+router.delete("/:id", async (req, res) => {
+	console.log("API comment route");
+	console.log(`Post ID: ${req.params.post_id}`);
+});
+
 export default router;
