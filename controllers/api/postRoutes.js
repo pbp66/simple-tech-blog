c967c57d9b68f72cbdc9209e24e978bf838eb84f
pbp66 <{ID}+{username}@users.noreply.github.com>
2022-12-19 22:05:09 -0500

    basic post routes complete

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,6 +5,7 @@ import { Comment, Post, User } from "../../models";
 router.get("/", async (req, res) => {
 	try {
 		const allPostsData = await Post.findAll({
+			include: [Comment, User],
 			order: [["updateAt", "DESC"]],
 		});
 
@@ -21,6 +22,45 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
 	try {
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(`<h1>500 Internal Server Error</h1>`);
+	}
+});
+
+router.post("/", async (req, res) => {
+	try {
+		const newPost = await Post.create(req.body);
+		res.status(201).json(newPost).send();
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(`<h1>500 Internal Server Error</h1>`);
+	}
+});
+
+router.put("/:id", async (req, res) => {
+	try {
+		const exists = await Post.findOne({ where: { id: req.params.id } });
+		if (exists) {
+			const updatedPost = await Post.update(
+				{
+					title: req.body.title,
+					content: req.body.content,
+				},
+				{ where: { id: req.params.id } }
+			);
+
+			// sequelize sends an array of length one with 1 if the entry was updated. Otherwise, it sends a 0 if the entry was not.
+			if (updatedPost[0]) {
+				res.status(204).send();
+			} else {
+				res.status(304).send();
+			}
+		} else {
+			res.status(400).send(
+				`<h1>400 Bad Request</h1><h3>Post does not exist. Create a post using a Post Route instead.</h3>`
+			);
+		}
 	} catch (err) {
 		console.error(err);
 		res.status(500).send(`<h1>500 Internal Server Error</h1>`);
