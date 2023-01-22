20a2ef5cad83822620bec8111cbd57483711336d
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-01-21 19:38:07 -0500

    updated login route to /home from /

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,7 +41,7 @@ router.get("/login", (req, res) => {
 
 	// If a session exists, redirect the request to the homepage
 	if (req.session.logged_in) {
-		res.redirect("/");
+		res.redirect("/home");
 		return;
 	}
 
